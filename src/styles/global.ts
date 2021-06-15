9947b0c363936a4475e83c63f653b92d31bfa268
mrLuisFer <mrluisfeer@gmail.com>
2021-06-15 17:38:01 -0500

    feat: Poppins font added

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,8 +19,7 @@ html {
 
 body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  font-family: var(--body-font);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   scroll-behavior: smooth;
