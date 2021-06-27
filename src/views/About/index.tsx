6e6e4604d98cf0c34e022c7b42f345521179e0e1
mrLuisFer <mrluisfeer@gmail.com>
2021-06-26 21:51:10 -0500

    feat: adding head or SEO in the views

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'src/components/Head/Head'
+
 export default function About() {
   return (
-    <div>
+    <main>
+      <Head
+        title='Blog | Acerca de'
+        description='Conoce mas acerca de este blog y su proposito! :D'
+        keyWords='Acerca de About Frontend Web Development Javascript React HTML CSS'
+      />
       <p>About</p>
-    </div>
+    </main>
   )
 }
--- src/views/BooksView/index.tsx
+++ src/views/BooksView/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'src/components/Head/Head'
+
 export default function BooksView() {
   return (
-    <div>
+    <main>
+      <Head
+        title='Blog | Libros gratis'
+        description='Lista de los mejores libros para aprender sobre programacion y de manera gratuita'
+        keyWords='Libros Libros Gratis Programacion Desarrollo Frontend Backend'
+      />
       <p>Books</p>
-    </div>
+    </main>
   )
 }
