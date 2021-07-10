88d2e24f3528c711ff5ab6639b273e772e1037af
mrLuisFer <mrluisfeer@gmail.com>
2021-07-09 20:58:05 -0500

    adding extends with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'standard', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -24,6 +24,7 @@ module.exports = {
     'react/react-in-jsx-scope': RULES.OFF,
     'react/jsx-uses-vars': RULES.ERROR,
     'react/prop-types': RULES.OFF,
+    eqeqeq: RULES.ERROR,
   },
   settings: {
     react: {
