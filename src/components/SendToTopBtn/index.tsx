918ff0daa0ce8e63dbccede9c3c68cf52bbaced1
mrLuisFer <mrluisfeer@gmail.com>
2021-08-19 00:24:05 -0500

    feat: send to top basic functionality added

--- src/components/SendToTopBtn/index.tsx
+++ src/components/SendToTopBtn/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react'
 import ArrowToTop from 'src/assets/icons/arrow-to-top.svg'
 import { BtnStyled } from './styles'
 
 export default function SendToTopBtn() {
+  const [isScroll, setIsScroll] = useState<boolean>(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop: number = document.documentElement.scrollTop
+      setIsScroll(scrollTop > 0)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  console.log(isScroll)
+
   return (
-    <BtnStyled>
+    <BtnStyled title='Volver al incio' isScroll={isScroll}>
       <img src={ArrowToTop} alt='Volver al incio' />
     </BtnStyled>
   )
--- src/components/SendToTopBtn/styles.ts
+++ src/components/SendToTopBtn/styles.ts
@@ -1,11 +1,25 @@
 import { cssVariables } from 'src/styles/utils/cssVariables'
 import styled from 'styled-components'
 
-export const BtnStyled = styled.button`
+type BtnStyledProps = {
+  isScroll: boolean
+}
+
+export const BtnStyled = styled.button<BtnStyledProps>`
   position: absolute;
-  width: 50px;
-  height: 50px;
+  width: 2rem;
+  height: 2rem;
   border-radius: 50%;
-  border: none;
-  background-color: ${cssVariables.darkGrey};
+  border: 0.3px solid ${cssVariables.darkGrey};
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  right: 1rem;
+  bottom: 10px;
+  cursor: pointer;
+  transition: ${cssVariables.transition};
+  display: ${(props) => (props.isScroll ? 'block' : 'none')};
+
+  &:hover {
+    width: 2.2rem;
+    height: 2.2rem;
+  }
 `
