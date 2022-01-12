deb7497763b8716999b16a2e4809adf4c5b2f906
mrLuisFer <mrluisfeer@gmail.com>
2022-01-11 23:04:41 -0600

    rename App dir to lowercase and add index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import './styles/index.css'
 import { render } from 'react-dom'
 import { StrictMode } from 'react'
-import App from './App/App'
+import App from './app'
 
-/**
- * null is required but cannot be
- * @param {HTMLElement} ROOT
- */
-const ROOT: HTMLElement | null = document.getElementById('root')
+const ROOT: HTMLElement = document.getElementById('root')!
 
 render(
   <StrictMode>
