53423c4e66a4ff63725ec61fd23df3d2f8ec96cf
mrLuisFer <mrluisfeer@gmail.com>
2022-02-05 20:33:51 -0600

    remove root variable

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,9 @@ import { render } from 'react-dom'
 import { StrictMode } from 'react'
 import App from './app'
 
-const ROOT: HTMLElement = document.getElementById('root')!
-
 render(
   <StrictMode>
     <App />
   </StrictMode>,
-  ROOT
+  document.getElementById('root')
 )
